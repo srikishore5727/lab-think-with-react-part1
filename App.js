ac37e9f7ecfded177c0942d25ecec5c867c5b4ae
srikishore5727 <srikishore9080676683@gmail.com>
2023-12-31 16:42:28 +0530

    done

---
+++ App.js
@@ -0,0 +1,11 @@
+class ReactContainer extends React.Component{
+  
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll with classes`)
+          );
+      }
+    
+  }
+  const container = document.getElementById('react-container');
+  ReactDOM.render(React.createElement(ReactContainer),container);
